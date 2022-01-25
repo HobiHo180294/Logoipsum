27330797f6b677699d83469312012c65741006b1
Egor Potebenko <hitechnic.uu68@gmail.com>
2022-01-25 22:52:29 +0200

    Adding burger menu, smooth scrolling to basic website sections. Creating full adaptive website

---
+++ src/js/main.js
@@ -0,0 +1,65 @@
+// Sticky header
+(function () {
+  const header = document.querySelector(".header");
+  window.onscroll = () => {
+    if (window.pageYOffset > 50) {
+      header.classList.add("header_active");
+    } else {
+      header.classList.remove("header_active");
+    }
+  };
+})();
+
+// Burger handler
+$(document).ready(function () {
+  $(".header__burger").click(function (event) {
+    $(".header__burger, .menu, .header__button").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
+});
+
+// Closing burger menu by clicking on the links
+$(document).ready(function () {
+  $(".menu__link, .header__button").click(function (event) {
+    $(".header__burger, .menu, .header__button").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
+});
+
+// Smooth scrolling to anchors
+(function () {
+  const smoothScroll = function (targetEl, duration) {
+    const headerElHeight = document.querySelector(".header").clientHeight;
+    let target = document.querySelector(targetEl);
+    let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+    let startPosition = window.pageYOffset;
+    let startTime = null;
+
+    const ease = function (t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return (c / 2) * t * t + b;
+      t--;
+      return (-c / 2) * (t * (t - 2) - 1) + b;
+    };
+
+    const animation = function (currentTime) {
+      if (startTime === null) startTime = currentTime;
+      const timeElapsed = currentTime - startTime;
+      const run = ease(timeElapsed, startPosition, targetPosition, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
+    };
+    requestAnimationFrame(animation);
+  };
+
+  const scrollTo = function () {
+    const links = document.querySelectorAll(".js-scroll");
+    links.forEach((each) => {
+      each.addEventListener("click", function () {
+        const currentTarget = this.getAttribute("href");
+        smoothScroll(currentTarget, 1000);
+      });
+    });
+  };
+  scrollTo();
+})();
